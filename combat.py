0e494aaed365e82061d6be13a0700be225859f67
Lazaros Bistas <mpistasl@csd.auth.gr>
2023-02-14 19:45:08 +0200

    I fixed the music paths so that they can play, they played the default windows error before

--- combat.py
+++ combat.py
@@ -1658,8 +1658,6 @@ def shop(): #Creating the shop function
             winsound.PlaySound(None, winsound.SND_ASYNC + winsound.SND_LOOP) #ends the store music
             break
 
-#test
-#test2
 def town(a): #creating a function for the town in the game
     global lv, gold, xp, lvup, hpot, hpots, spot, spots, mpot, mpots #obtaining important variables needed for the function from the global pool
     while True: #Looping endlessly until the player chooses to continue the story
